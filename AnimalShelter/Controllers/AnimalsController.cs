55700998e765cdaa96a376b461f8f4694eb16e13
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-12 14:01:52 -0700

    populate animalscontroller

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -12,37 +12,84 @@ namespace AnimalShelter.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
+        private AnimalShelterContext _db;
 
-        // GET api/values
+        public AnimalsController(AnimalShelterContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/animals
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Animal>> Get(string name, string species, string family, string gender, string mass, string diet, string period, string preferredfood)
         {
-            return new string[] { "value1", "value2" };
+            var query = _db.Animals.AsQueryable();
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+            if (species != null)
+            {
+                query = query.Where(entry => entry.Species == species);
+            }
+            if (family != null)
+            {
+                query = query.Where(entry => entry.Family == family);
+            }
+            if (gender != null)
+            {
+                query = query.Where(entry => entry.Gender == gender);
+            }
+            if (mass != null)
+            {
+                query = query.Where(entry => entry.Mass == mass);
+            }
+            if (diet != null)
+            {
+                query = query.Where(entry => entry.Diet == diet);
+            }
+            if (period != null)
+            {
+                query = query.Where(entry => entry.Period == period);
+            }
+            if (preferredfood != null)
+            {
+                query = query.Where(entry => entry.PreferredFood == preferredfood);
+            }
+            return query.ToList();
         }
 
-        // GET api/values/5
+        // GET api/animals/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Animal> Get(int id)
         {
-            return "value";
+            return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
         }
 
-        // POST api/values
+        // POST api/animals
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Animal animal)
         {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
         }
 
-        // PUT api/values/5
+        // PUT api/animals/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Animal animal)
         {
+            animal.AnimalId = id;
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
         }
 
-        // DELETE api/values/5
+        // DELETE api/animals/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+            _db.Animals.Remove(animalToDelete);
+            _db.SaveChanges();
         }
     }
 }
